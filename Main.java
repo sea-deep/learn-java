f2d14e15a7c2b64b69591804e1dcbfaee98eec3d
sea-deep <seadeepie@gmail.com>
2026-09-07 21:37:09 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("hello world");
-        System.out.println("!!");
-        System.out.println("testttt");
+        // System.out.print("hello world");
+        // System.out.println("!!");
+        // System.out.println("testttt");
+        String firstName = "Dipak";
+        String lastName = "Agrahari";
+        int age= 19;
+        float twelfth = 72.8;
+        System.out.printf("Mera naam %s %s hai and mere 12th me %f% number aaye the... \n and meri age is %d", firstName,lastName, twelfth, age);
+
     }
 }
